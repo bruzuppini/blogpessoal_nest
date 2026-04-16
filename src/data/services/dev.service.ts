6fc6e939fd5ecf99d6c1f5efeaed6e8a32723669
Bruna <brunazuppini@gmail.com>
2026-04-16 12:44:08 -0300

    Deploy do Projeto Blog Pessoal

--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
-import { Tema } from "../../tema/entities/tema.entity";
+import { Tema } from "../../postagem/tema/entities/tema.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
 
 @Injectable()
